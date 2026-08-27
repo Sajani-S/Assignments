e971c5cfe52de3dc9e58f670fd48b049a476acd0
sajani <sangaiansangain@gmail.com>
2026-08-27 10:48:31 +0530

    Added ifelse.java to Conditional Statement

---
+++ Conditional Statement/ifelse.java
@@ -0,0 +1,45 @@
+public class ifelse {
+
+    public static void main (String[] args){
+
+        int sky = 21;
+
+        if(sky % 2 == 0 )
+
+            System.out.println("the given number is even");
+
+          else
+              System.out.println("the given number is  odd");
+
+
+          if ( sky < 0 )
+
+              System.out.println(" the given number is negative ");
+
+           else if ( sky > 0)
+
+               System.out.println( "the given number is positive" );
+
+
+            else
+
+                System.out.println("the given number is zero");
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
