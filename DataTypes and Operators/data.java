24cd2053ac55f433f075fb40c6035f033c5f59a0
sajani <sangaiansangain@gmail.com>
2026-08-27 10:11:39 +0530

    Added data.java to DataTypes and Operators

---
+++ DataTypes and Operators/data.java
@@ -0,0 +1,40 @@
+
+
+
+               public class data {
+
+               public static void main ( String [] args){
+
+                int venus = 2;
+                float earth = 5 ;
+                double sun = 300.1;
+                char moon = 's';
+                boolean star = true ;
+                String mercury = "mystery";
+
+                System.out.println("integer : " + venus);
+                System.out.println("float : " + earth);
+                System.out.println("double : " + sun);
+                System.out.println("char : " + moon);
+                System.out.println("boolean;" + star);
+                System.out.println("string:" + mercury);
+
+
+
+               int saj = 3;
+                double sam = saj ;
+                double raj = 21.5;
+                int nim = (int)raj;
+                System.out.println("double : " + sam);
+                System.out.println("int : " + nim);
+
+
+
+
+    }
+
+
+
+
+
+}
