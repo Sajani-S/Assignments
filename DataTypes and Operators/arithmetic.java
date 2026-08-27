48e9a1933b86667810d3d13835407d68f893f41b
sajani <sangaiansangain@gmail.com>
2026-08-27 10:24:51 +0530

    Added arithmetic.java to DataTypes and Operators

---
+++ DataTypes and Operators/arithmetic.java
@@ -0,0 +1,42 @@
+public class arithmetic {
+
+    public static void main (String[] args) {
+
+        int a = 18 , b = 3 ;
+        int summer = a + b;
+        int winter = a - b ;
+        int autumn = a * b ;
+        int rainy = a / b ;
+        int spring = a % b ;
+
+        System.out.println("addition :" + summer );
+        System.out.println("subraction :" + winter );
+        System.out.println("multiplicatioon :" + autumn );
+        System.out.println("division :" + rainy );
+        System.out.println("modulas :" + spring );
+
+        int s = 10 ;
+        s += 2;
+        System.out.println("sum : " + s);
+         s -= 3;
+        System.out.println("diff : " + s);
+          s *= 4;
+        System.out.println("multi : " + s);
+        s /= 5;
+        System.out.println("div : " + s);
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+}
